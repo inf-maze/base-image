110c663a21c1dd50e26267dd4addab33f484d15f
Expo Tao <expo.tao@gmail.com>
2019-12-31 23:02:15 +0800

    MOD add date to log of zombie-reaping-problem

--- problems/zombie-reaping-problem.c
+++ problems/zombie-reaping-problem.c
@@ -6,11 +6,31 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdarg.h>
+#include <time.h>
+
+void write_log(const char *format, ...) {
+    time_t timer;
+    char buffer[26];
+    struct tm* tm_info;
+
+    time(&timer);
+    tm_info = localtime(&timer);
+
+    strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", tm_info);
+
+    printf("%s ", buffer);
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+    printf("\n");
+}
 
 void signal_handler(int signal) {
     switch(signal) {
         case SIGCHLD:
-            printf("received SIGCHLD\n");
+            write_log("Got SIGCHLD.");
             break;
 	}
 }
@@ -33,7 +53,7 @@ int main(int argc, char **argv) {
     snprintf(argv[0], argv0size, "%s", "main");
 
     if (signal(SIGCHLD, signal_handler) == SIG_ERR)
-        printf("can't trap SIGCHLD\n");
+        write_log("can't trap SIGCHLD.");
 
     pid_t zombie_parent_pid = fork();
     if(zombie_parent_pid == 0) {
@@ -45,21 +65,21 @@ int main(int argc, char **argv) {
                 // name zombie process
                 memset(argv[0], 0, argv0size);
                 snprintf(argv[0], argv0size, "%s-%d", "zombie", i);
-                printf("process zombie-%d will exit after %d seconds.\n", i, 10 + i * 5);
+                write_log("process zombie-%d will exit after %d seconds.", i, 10 + i * 5);
                 sleep(10 + i * 5);
-                printf("process zombie-%d exit.\n", i);
+                write_log("process zombie-%d exit.", i);
                 exit(0);
             }
         }
-        printf("zombie processes have been created, zombie-parent process will exit after 10 seconds.\n");
+        write_log("zombie processes have been created, zombie-parent process will exit after 10 seconds.");
         sleep(10);
         exit(0);
     }
 
     waitpid(zombie_parent_pid, NULL, 0);
-    printf("main process will exit after 300 seconds.\n");
+    write_log("main process will exit after 300 seconds.");
     for(int i = 0; i < 300; ++i)
         sleep(1);
-    printf("main process exit.\n");
+    write_log("main process exit.");
     return 0;
 }
